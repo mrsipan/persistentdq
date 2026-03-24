900bbb039aeb2253098ab2f6f62b9987ef0f56af
ben.sanchez <ben.sanchez@mastercard.com>
2026-03-24 18:39:48 -0400

    Add files

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
-import persistent
+import setuptools
+
+setuptools.setup(
+    name="persistentdq",
+    version="0.0.1",
+    description="persistentdq",
+    author="Benjamin Sanchez",
+    py_modules=["zonzo"],
+    package_dir={"": "."},
+    install_requires=[],
+    python_requires=">=3.8",
+    )
